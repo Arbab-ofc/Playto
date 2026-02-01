1d111b75d84c4fcc0dfe8ee5f364c8709df95f73
arbabprvt-ofc <arbabprvt@gmail.com>
2026-02-01 22:45:06 +0530

    feat: block auth pages when logged in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,29 @@ import { Profile } from './pages/Profile';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
 import { ProtectedRoute } from './components/auth/ProtectedRoute';
+import { PublicRoute } from './components/auth/PublicRoute';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoute>
+              <Register />
+            </PublicRoute>
+          }
+        />
         <Route
           path="/profile"
           element={
